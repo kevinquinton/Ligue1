fc8f6fac33226bd0d6717961c9888bbd0ed6ae52
quintonk <quintonk@FR-L2200535>
2017-02-13 14:44:00 +0100

    Ajoutez des fichiers projet.

---
+++ Ligue1/Entities/EntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Ligue1.Entities
+{
+    public abstract class EntityBase : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Ligue1/Entities/Team.cs
@@ -0,0 +1,22 @@
+﻿namespace Ligue1.Entities
+{
+    public class Team : EntityBase
+    {
+        private string _name;
+
+        public Team() { }
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+
+    }
+}
